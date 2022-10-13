948e68071b5597f7a3924cd1fd36798a2c63d1d1
youssefahmed55 <joussefahmed2012@gmail.com>
2022-10-13 05:40:03 +0200

    Update : Using Real DAO in RemindersLocalRepositoryTest

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -2,6 +2,7 @@ package com.udacity.project4.locationreminders.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
@@ -34,7 +35,7 @@ class RemindersLocalRepositoryTest {
 
     @Before
     fun setupRepo() {
-        remindersDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().context, RemindersDatabase::class.java).build()    //Initialize remindersDatabase (RemindersDatabase)
+        remindersDatabase = Room.inMemoryDatabaseBuilder(ApplicationProvider.getApplicationContext(), RemindersDatabase::class.java).build()    //Initialize remindersDatabase (RemindersDatabase)
         remindersDaoo = remindersDatabase.reminderDao()    //Initialize remindersDaoo
         remindersRepository = RemindersLocalRepository(remindersDaoo, Dispatchers.Unconfined) //Initialize remindersRepository
 
